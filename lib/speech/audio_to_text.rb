9cb784c4a97f1e0e475257d4823295ff5d590eb8
Jon Schewe <jpschewe@mtu.net>
2011-04-10 11:33:59 -0500

    Decrease default chunk size to 3

--- lib/speech/audio_to_text.rb
+++ lib/speech/audio_to_text.rb
@@ -12,7 +12,7 @@ module Speech
 
     def to_text
       url = "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=speech2text&lang=en-US&maxresults=10"
-      splitter = Speech::AudioSplitter.new(file) # based off the wave file because flac doesn't tell us the duration
+      splitter = Speech::AudioSplitter.new(file, 3) # based off the wave file because flac doesn't tell us the duration
       easy = Curl::Easy.new(url)
       splitter.split.each do|chunk|
         chunk.build.to_flac
